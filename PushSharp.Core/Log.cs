711c2f7ef4fb2a681be014899ab91d0297fa8d0f
bmatson <bmatson>
2013-07-15 13:46:53 +0200

    Added logging facade, additional log messages in APNS.

--- PushSharp.Core/Log.cs
+++ PushSharp.Core/Log.cs
@@ -5,28 +5,47 @@ using System.Text;
 
 namespace PushSharp.Core
 {
-	public class Log
+    public class Log
 	{
 		public static LogLevel Level = LogLevel.None;
+        public static ILogger Logger { get; set; }
+
+        static Log() {
+            Logger = null;
+        }
 
 		public static void Info(string format, params object[] objs)
 		{
-			if (((int)Level) >= ((int)LogLevel.Info))
-				Console.WriteLine("INFO [" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] " + format, objs);
+            if (((int)Level) >= ((int)LogLevel.Info))
+            {
+                if (Logger == null)
+                    Console.WriteLine("INFO [" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] " + format, objs);
+                else
+                    Logger.Info(format, objs);
+            }
 		}
 
 		public static void Warning(string format, params object[] objs)
 		{
 			if (((int)Level) >= ((int)LogLevel.Warning))
-				Console.WriteLine("WARN [" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] " + format, objs);
-		}
+            {
+                if (Logger == null) 
+				    Console.WriteLine("WARN [" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] " + format, objs);
+                else
+                    Logger.Warning(format, objs);
+            }
+        }
 
 		public static void Error(string format, params object[] objs)
 		{
-			if (((int)Level) >= ((int)LogLevel.Error))
-				Console.WriteLine("ERR  [" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] " + format, objs);
+            if (((int)Level) >= ((int)LogLevel.Error))
+            {
+                if (Logger == null)
+                    Console.WriteLine("ERR  [" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] " + format, objs);
+                else
+                    Logger.Error(format, objs);
+            }
 		}
-
 	}
 
 	public enum LogLevel
